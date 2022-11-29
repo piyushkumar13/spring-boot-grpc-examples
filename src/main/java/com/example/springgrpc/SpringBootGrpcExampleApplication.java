a1dd6cab1e7bc2afa992b1c72155167d29068335
pkumar <piyush.kumar@logmein.com>
2022-11-29 18:45:31 +0530

    initial commit

---
+++ src/main/java/com/example/springgrpc/SpringBootGrpcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGrpcExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGrpcExampleApplication.class, args);
+    }
+
+}
